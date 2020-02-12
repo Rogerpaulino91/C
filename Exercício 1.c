b616f47904dfa66e8abc789dc8f95a5dbb969244
Rogerpaulino91 <roger_12_937@hotmail.com>
2020-02-11 21:29:25 -0300

    commit

---
+++ Exercício 1.c
@@ -0,0 +1,21 @@
+// O coração humano bate em média uma vez por segundo. //
+// Desenvolver um algoritmo para calcular e escrever quantas vezes o coração de uma pessoa baterá se viver X anos. //
+
+/*#include<studio.h>
+#include<math.h>
+#define batidaano 365.25
+#define batidadia 24
+#define batidahora 60
+#define batidaminuto 60 
+
+int main () 
+
+{
+		int idade; 
+		float batotal; 
+		scanf("%d",idade);
+		battotal = batidaano * batidadia* batidahora * batidaminuto * idade;
+		printf("O CORACAO BATEU %.2f VEZES\n",batidatotal);
+		return 0;
+		
+}
