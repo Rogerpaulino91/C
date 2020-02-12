78348d137a5ebf00b9e15b752633bd8a5cf45129
Rogerpaulino91 <roger_12_937@hotmail.com>
2020-02-11 21:38:44 -0300

    Create Exercício 2.c

---
+++ Exercício 2.c
@@ -0,0 +1,21 @@
+// Um Trabalhador catador de latas deseja desenvolver um algoritimo para calcular o lucro por arrecadação de latas. //
+// sabendo-se que o lucro do alumínio por kg é R$ 10,00. 
+
+/*include<stdio.h>
+#include<math.h>
+#define valorkg 10
+
+int main () 
+
+{ 
+
+	float peso,valortotal; 
+
+	scanf("%f,"peso);
+	
+	valortotal = peso * valorkg;
+
+	printf("O VALOR DO LUCRO E = %.2f\n",valor total); 
+
+
+	}
