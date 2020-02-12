a179d9c4fe6a3ce71a219b1b391a0f89befb9848
Rogerpaulino91 <44624047+Rogerpaulino91@users.noreply.github.com>
2020-02-12 12:53:58 -0300

    Update Exercício 2.c

--- Exercício 2.c
+++ Exercício 2.c
@@ -1,7 +1,7 @@
 // Um Trabalhador catador de latas deseja desenvolver um algoritimo para calcular o lucro por arrecadação de latas. //
 // sabendo-se que o lucro do alumínio por kg é R$ 10,00. 
 
-/*include<stdio.h>
+#include<stdio.h>
 #include<math.h>
 #define valorkg 10
 
